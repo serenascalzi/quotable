c2e88838702c448e649a24f624623c3ac9a7f201
serenascalzi <serenascalzi@alumni.iu.edu>
2020-01-01 18:02:48 -0800

    test workflow

--- js/quotes.js
+++ js/quotes.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 	// Random Varied Quote
 	let variedArray = [
-		// December 31
+		// December 31 (TEST)
 		'&ldquo;Be at war with your vices, at peace with your neighbors, and let every new year find you a better man.&rdquo; <br /> &mdash;Benjamin Franklin',
         // January 1
         '&ldquo;I think in terms of the day&rsquo;s resolutions, not the years&rsquo;.&rdquo; <br /> &mdash;Henry Moore'
@@ -21,7 +21,7 @@ $(document).ready(function() {
 
     // Random Love Quote
     let loveArray = [
-		// December 31
+		// December 31 (TEST)
 		'&ldquo;With our love, we could save the world.&rdquo; <br /> &mdash;George Harrison',
         // January 1
         '&ldquo;The giving of love is an education in itself.&rdquo; <br /> &mdash;Eleanor Roosevelt'
@@ -39,7 +39,7 @@ $(document).ready(function() {
 
     // Random Art Quote
     let artArray = [
-		// December 31
+		// December 31 (TEST)
 		'&ldquo;The job of the artist is always to deepen the mystery.&rdquo; <br /> &mdash;Francis Bacon',
         // January 1
         '&ldquo;I make pictures and someone comes in and calls it art.&rdquo; <br /> &mdash;Willem de Kooning'
