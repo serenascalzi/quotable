7070014715f5bb5a3b08b5dff6cb1e9bc01e7726
serenascalzi <serenascalzi@alumni.iu.edu>
2020-01-02 09:26:19 -0800

    add january 2 quotes

--- js/quotes.js
+++ js/quotes.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // Month Day
 	// '&ldquo;Quote&rdquo; <br /> &mdash;Attribution'
 
 	// Random Varied Quote
@@ -6,7 +7,9 @@ $(document).ready(function() {
 		// December 31 (TEST)
 		'&ldquo;Be at war with your vices, at peace with your neighbors, and let every new year find you a better man.&rdquo; <br /> &mdash;Benjamin Franklin',
         // January 1
-        '&ldquo;I think in terms of the day&rsquo;s resolutions, not the years&rsquo;.&rdquo; <br /> &mdash;Henry Moore'
+        '&ldquo;I think in terms of the day&rsquo;s resolutions, not the years&rsquo;.&rdquo; <br /> &mdash;Henry Moore',
+        // January 2
+        '&ldquo;The sweetest of all sounds is praise.&rdquo; <br /> &mdash;Xenophon'
     ]
 
     let variedQuotes = variedArray.sort(function(a,b) {
@@ -24,7 +27,9 @@ $(document).ready(function() {
 		// December 31 (TEST)
 		'&ldquo;With our love, we could save the world.&rdquo; <br /> &mdash;George Harrison',
         // January 1
-        '&ldquo;The giving of love is an education in itself.&rdquo; <br /> &mdash;Eleanor Roosevelt'
+        '&ldquo;The giving of love is an education in itself.&rdquo; <br /> &mdash;Eleanor Roosevelt',
+        // January 2
+        '&ldquo;Dear Lord, I&rsquo;m so grateful I&rsquo;m still loved.&rdquo; <br /> &mdash;Vivien Leigh'
     ]
 
     let loveQuotes = loveArray.sort(function(a,b) {
@@ -42,7 +47,9 @@ $(document).ready(function() {
 		// December 31 (TEST)
 		'&ldquo;The job of the artist is always to deepen the mystery.&rdquo; <br /> &mdash;Francis Bacon',
         // January 1
-        '&ldquo;I make pictures and someone comes in and calls it art.&rdquo; <br /> &mdash;Willem de Kooning'
+        '&ldquo;I make pictures and someone comes in and calls it art.&rdquo; <br /> &mdash;Willem de Kooning',
+        // January 2
+        '&ldquo;I think it is in collaboration that the nature of art is revealed.&rdquo; <br /> &mdash;Steve Lacy'
     ]
 
     let artQuotes = artArray.sort(function(a,b) {
