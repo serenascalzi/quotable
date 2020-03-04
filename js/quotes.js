12443d3e636f2f64aecfeae0208a0b561a991164
serenascalzi <serenascalzi@alumni.iu.edu>
2020-03-03 19:27:28 -0800

    add march 3 quotes

--- js/quotes.js
+++ js/quotes.js
@@ -7,7 +7,9 @@ $(document).ready(function() {
         // March 1
         '&ldquo;Everything&rsquo;s got a moral, if only you can find it.&rdquo; <br /> &mdash;Lewis Carroll',
         // March 2
-        '&ldquo;The whole secret of life is to be interested in one thing profoundly and in a thousand things well.&rdquo; <br /> &mdash;Horace Walpole'
+        '&ldquo;The whole secret of life is to be interested in one thing profoundly and in a thousand things well.&rdquo; <br /> &mdash;Horace Walpole',
+        // March 3
+        '&ldquo;Everything has been figured out, expect how to live.&rdquo; <br /> &mdash;Jean-Paul Sartre'
     ]
 
     let variedQuotes = variedArray.sort(function(a,b) {
@@ -25,7 +27,9 @@ $(document).ready(function() {
         // March 1
         '&ldquo;People who throw kisses are hopelessly lazy.&rdquo; <br /> &mdash;Bob Hope',
         // March 2
-        '&ldquo;Who, being loved, is poor?&rdquo; <br /> &mdash;Oscar Wilde'
+        '&ldquo;Who, being loved, is poor?&rdquo; <br /> &mdash;Oscar Wilde',
+        // March 3
+        '&ldquo;We waste time looking for the perfect lover, instead of creating the perfect love.&rdquo; <br /> &mdash;Tom Robbins'
     ]
 
     let loveQuotes = loveArray.sort(function(a,b) {
@@ -43,7 +47,9 @@ $(document).ready(function() {
         // March 1
         '&ldquo;Every picture shows a spot with which the artist has fallen in love.&rdquo; <br /> &mdash;Alfred Sisley',
         // March 2
-        '&ldquo;An empty canvas is full.&rdquo; <br /> &mdash;Robert Rauschenberg'
+        '&ldquo;An empty canvas is full.&rdquo; <br /> &mdash;Robert Rauschenberg',
+        // March 3
+        '&ldquo;Rationalism is the enemy of art, though necessary as a basis for architecture.&rdquo; <br /> &mdash;Arthur Erickson'
     ]
 
     let artQuotes = artArray.sort(function(a,b) {
