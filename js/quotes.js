5c1a391ebee044e102c23a3ffb8eb76346c04b87
serenascalzi <serenascalzi@alumni.iu.edu>
2020-05-02 12:35:41 -0700

    add may 2 quotes

--- js/quotes.js
+++ js/quotes.js
@@ -5,7 +5,9 @@ $(document).ready(function() {
 	// Random Varied Quote
 	let variedArray = [
         // May 1
-        '&ldquo;Forgiveness is a funny thing. It warms the heart and cools the sting.&rdquo; <br /> &mdash;William Arthur Ward'
+        '&ldquo;Forgiveness is a funny thing. It warms the heart and cools the sting.&rdquo; <br /> &mdash;William Arthur Ward',
+        // May 2
+        '&ldquo;I hear, I know. I see, I remember. I do, I understand.&rdquo; <br /> &mdash;Confucius'
     ]
 
     let variedQuotes = variedArray.sort(function(a,b) {
@@ -21,7 +23,9 @@ $(document).ready(function() {
     // Random Love Quote
     let loveArray = [
         // May 1
-        '&ldquo;Morning without you is a dwindled dawn.&rdquo; <br /> &mdash;Emily Dickinson'
+        '&ldquo;Morning without you is a dwindled dawn.&rdquo; <br /> &mdash;Emily Dickinson',
+        // May 2
+        '&ldquo;Life without love is like a tree without blossoms or fruit.&rdquo; <br /> &mdash;Khalil Gibran'
     ]
 
     let loveQuotes = loveArray.sort(function(a,b) {
@@ -37,7 +41,9 @@ $(document).ready(function() {
     // Random Art Quote
     let artArray = [
         // May 1
-        '&ldquo;Art is a marriage of the conscious and the unconscious.&rdquo; <br /> &mdash;Jean Cocteau'
+        '&ldquo;Art is a marriage of the conscious and the unconscious.&rdquo; <br /> &mdash;Jean Cocteau',
+        // May 2
+        '&ldquo;Art is either plagiarism or revolution.&rdquo; <br /> &mdash;Paul Gauguin'
     ]
 
     let artQuotes = artArray.sort(function(a,b) {
