0bd3abfd3fad61115b813acf172282b7d05a9537
serenascalzi <serenascalzi@alumni.iu.edu>
2020-08-05 09:02:43 -0700

    add august 5 quotes

--- js/quotes.js
+++ js/quotes.js
@@ -11,7 +11,9 @@ $(document).ready(function() {
         // August 3
         '&ldquo;I have no methods; all I do is accept people as they are.&rdquo; <br /> &mdash;Joan Rivers',
         // August 4
-        '&ldquo;I am prepared for the worst, but hope for the best.&rdquo; <br /> &mdash;Benjamin Disraeli'
+        '&ldquo;I am prepared for the worst, but hope for the best.&rdquo; <br /> &mdash;Benjamin Disraeli',
+        // August 5
+        '&ldquo;Dreams will get you nowhere, a good kick in the pants will take you a long way.&rdquo; <br /> &mdash;Baltasar Gracian'
     ]
 
     let variedQuotes = variedArray.sort(function(a,b) {
@@ -33,7 +35,9 @@ $(document).ready(function() {
         // August 3
         '&ldquo;Marriages, like careers, need constant nurturing&hellip; the secret of having it all is loving it all.&rdquo; <br /> &mdash;Joyce Brothers',
         // August 4
-        '&ldquo;It is better to have loved and lost than never to have lost at all.&rdquo; <br /> &mdash;Samuel Butler'
+        '&ldquo;It is better to have loved and lost than never to have lost at all.&rdquo; <br /> &mdash;Samuel Butler',
+        // August 5
+        '&ldquo;Blessed are the hearts that can bend; they shall never be broken.&rdquo; <br /> &mdash;Albert Camus'
     ]
 
     let loveQuotes = loveArray.sort(function(a,b) {
@@ -55,7 +59,9 @@ $(document).ready(function() {
         // August 3
         '&ldquo;Form follows function.&rdquo; <br /> &mdash;Louis Sullivan',
         // August 4
-        '&ldquo;To make us feel small in the right way is a function of art; men can only make us feel small in the wrong way.&rdquo; <br /> &mdash;E. M. Forster'
+        '&ldquo;To make us feel small in the right way is a function of art; men can only make us feel small in the wrong way.&rdquo; <br /> &mdash;E. M. Forster',
+        // August 5
+        '&ldquo;A great artist is always before his time or behind it.&rdquo; <br /> &mdash;George Edward Moore'
     ]
 
     let artQuotes = artArray.sort(function(a,b) {
