3bf90018258946b00312d01c3fac1c2261e4b466
serenascalzi <serenascalzi@alumni.iu.edu>
2021-01-03 16:38:26 -0800

    add january 3 quotes

--- js/funnyQuotes.js
+++ js/funnyQuotes.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
 
     let januaryArray = [
         '&ldquo;An intellectual is a man who doesn&rsquo;t know how to park a bike.&rdquo; <br /> &mdash;Spiro T. Agnew',
-        '&ldquo;Prediction is very difficult, especially if it&rsquo;s about the future.&rdquo; <br /> &mdash;Niels Bohr'
+        '&ldquo;Prediction is very difficult, especially if it&rsquo;s about the future.&rdquo; <br /> &mdash;Niels Bohr',
+        '&ldquo;Unemployment is capitalism&rsquo;s way of getting you to plant a garden.&rdquo; <br /> &mdash;Orson Scott Card'
     ]
 
     let februaryArray = [
