706ee63281e9776aa5b9b8c9eab4584d4602545b
serenascalzi <serenascalzi@alumni.iu.edu>
2021-01-11 18:18:32 -0800

    add january 11 quotes

--- js/natureQuotes.js
+++ js/natureQuotes.js
@@ -11,7 +11,8 @@ $(document).ready(function() {
         '&ldquo;My boy, one small breeze doesn&rsquo;t make a wind storm.&rdquo; <br /> &mdash;John McGraw',
         '&ldquo;If people think nature is their friend, then they sure don&rsquo;t need an enemy.&rdquo; <br /> &mdash;Kurt Vonnegut',
         '&ldquo;The fall of dropping water wears away the Stone.&rdquo; <br /> &mdash;Lucretius',
-        '&ldquo;We won&rsquo;t have a society if we destroy the environment.&rdquo; <br /> &mdash;Margaret Mead'
+        '&ldquo;We won&rsquo;t have a society if we destroy the environment.&rdquo; <br /> &mdash;Margaret Mead',
+        '&ldquo;Sunlight is painting.&rdquo; <br /> &mdash;Nathaniel Hawthorne'
     ]
 
     let februaryArray = [
