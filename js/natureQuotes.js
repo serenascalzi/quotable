d8bb112d35b773b071892f1823177fa07109265c
serenascalzi <serenascalzi@alumni.iu.edu>
2021-01-23 17:04:44 -0800

    add january 23 quotes

--- js/natureQuotes.js
+++ js/natureQuotes.js
@@ -23,7 +23,8 @@ $(document).ready(function() {
         '&ldquo;If the Sun and Moon should ever doubt, they&rsquo;d immediately go out.&rdquo; <br /> &mdash;William Blake',
         '&ldquo;The day, water, sun, moon, night &ndash; I do not have to purchase these things with money.&rdquo; <br /> &mdash;Plautus',
         '&ldquo;Men stumble over pebbles, never over mountains.&rdquo; <br /> &mdash;Marilyn French',
-        '&ldquo;That which is not good for the bee-hive cannot be good for the bees.&rdquo; <br /> &mdash;Marcus Aurelius'
+        '&ldquo;That which is not good for the bee-hive cannot be good for the bees.&rdquo; <br /> &mdash;Marcus Aurelius',
+        '&ldquo;For greed all nature is too little.&rdquo; <br /> &mdash;Lucius Annaeus Seneca'
     ]
 
     let februaryArray = [
