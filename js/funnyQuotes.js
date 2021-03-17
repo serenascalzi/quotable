8b10a6daa5e27487a39638ae7e884e198d8f2aeb
serenascalzi <serenascalzi@alumni.iu.edu>
2021-03-16 22:02:58 -0700

    add march 16 quotes

--- js/funnyQuotes.js
+++ js/funnyQuotes.js
@@ -16,7 +16,8 @@ $(document).ready(function() {
         '&ldquo;A woman is like a tea bag &ndash; you can&rsquo;t tell how strong she is until you put her in hot water.&rdquo; <br /> &mdash;Eleanor Roosevelt',
         '&ldquo;The better I get to know men, the more I find myself loving dogs.&rdquo; <br /> &mdash;Charles de Gaulle',
         '&ldquo;Ask five economists and you&rsquo;ll get five different answers &ndash; six if one went to Harvard.&rdquo; <br /> &mdash;Edgar Fiedler',
-        '&ldquo;Camping is nature&rsquo;s way of promoting the motel business.&rdquo; <br /> &mdash;Dave Barry'
+        '&ldquo;Camping is nature&rsquo;s way of promoting the motel business.&rdquo; <br /> &mdash;Dave Barry',
+        '&ldquo;I can speak Esperanto like a native.&rdquo; <br /> &mdash;Spike Milligan'
     ]
 
     let aprilArray = [
