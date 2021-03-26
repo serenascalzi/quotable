0eae02312ce1ef12e8552adc88f4071c8ed54cdc
serenascalzi <serenascalzi@alumni.iu.edu>
2021-03-26 11:56:32 -0700

    add march 26 quotes

--- js/funnyQuotes.js
+++ js/funnyQuotes.js
@@ -26,7 +26,8 @@ $(document).ready(function() {
         '&ldquo;I was a vegetarian until I started leaning toward the sunlight.&rdquo; <br /> &mdash;Rita Rudner',
         '&ldquo;Bachelors know more about women than married men; if they didn&rsquo;t they&rsquo;d be married too.&rdquo; <br /> &mdash;H. L. Mencken',
         '&ldquo;Lead us not into temptation. Just tell us where it is; we&rsquo;ll find it.&rdquo; <br /> &mdash;Sam Levenson',
-        '&ldquo;Hell hath no fury like a bureaucrat scorned.&rdquo; <br /> &mdash;Milton Friedman'
+        '&ldquo;Hell hath no fury like a bureaucrat scorned.&rdquo; <br /> &mdash;Milton Friedman',
+        '&ldquo;Middle age is when a guy keeps turning off lights for economical rather than romantic reasons.&rdquo; <br /> &mdash;Lillian Gordy Carter'
     ]
 
     let aprilArray = [
