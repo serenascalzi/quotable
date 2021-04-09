71a20917b6884770bce56d4e0b5c0861319919b0
serenascalzi <serenascalzi@alumni.iu.edu>
2021-04-09 14:11:31 -0700

    add april 9 quotes

--- js/natureQuotes.js
+++ js/natureQuotes.js
@@ -9,7 +9,8 @@ $(document).ready(function() {
         '&ldquo;The sea complains upon a thousand shores.&rdquo; <br /> &mdash;Alexander Smith',
         '&ldquo;The best thing one can do when it&rsquo;s raining is to let it rain.&rdquo; <br /> &mdash;Henry Wadsworth Longfellow',
         '&ldquo;Heaven is under our feet as well as over our heads.&rdquo; <br /> &mdash;Henry David Thoreau',
-        '&ldquo;The fulness of the godhead dwelt in every blade of grass.&rdquo; <br /> &mdash;Elias Hicks'
+        '&ldquo;The fulness of the godhead dwelt in every blade of grass.&rdquo; <br /> &mdash;Elias Hicks',
+        '&ldquo;Great men are like eagles, and build their nest on some lofty solitude.&rdquo; <br /> &mdash;Arthur Schopenhauer'
     ]
 
     let mayArray = [
