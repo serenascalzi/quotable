6d5f7679fd638c4f680bf67e49287d6cffaed1f5
serenascalzi <serenascalzi@alumni.iu.edu>
2021-07-23 11:23:45 -0700

    add july 23 quotes

--- js/funnyQuotes.js
+++ js/funnyQuotes.js
@@ -23,7 +23,8 @@ $(document).ready(function() {
         '&ldquo;I have had more trouble with myself than with any other man.&rdquo; <br /> &mdash;Dwight L. Moody',
         '&ldquo;Never trust anything that can think for itself if you can&rsquo;t see where it keeps its brain.&rdquo; <br /> &mdash;J. K. Rowling',
         '&ldquo;If you think it&rsquo;s hard to meet new people, try picking up the wrong golf ball.&rdquo; <br /> &mdash;Jack Lemmon',
-        '&ldquo;I drink to make other people interesting.&rdquo; <br /> &mdash;George Jean Nathan'
+        '&ldquo;I drink to make other people interesting.&rdquo; <br /> &mdash;George Jean Nathan',
+        '&ldquo;This stammer got me a home in Beverly Hills, and I&rsquo;m not about to screw with it now.&rdquo; <br /> &mdash;Bob Newhart'
     ]
 
     let augustArray = [
