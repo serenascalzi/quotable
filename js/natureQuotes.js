d53818aa5d5d94e3a71406575f282a03b9b5a57a
serenascalzi <serenascalzi@alumni.iu.edu>
2021-08-04 19:04:47 -0700

    add july 31 quotes

--- js/natureQuotes.js
+++ js/natureQuotes.js
@@ -31,7 +31,8 @@ $(document).ready(function() {
         '&ldquo;Don&rsquo;t tell me the moon is shining; show me the glint of light on broken glass.&rdquo; <br /> &mdash;Anton Chekhov',
         '&ldquo;Time will pass and seasons will come and go.&rdquo; <br /> &mdash;Roy Bean',
         '&ldquo;Ah, summer, what power you have to make us suffer and like it.&rdquo; <br /> &mdash;Russell Baker',
-        '&ldquo;In nature there are few sharp lines.&rdquo; <br /> &mdash;A. R. Ammons'
+        '&ldquo;In nature there are few sharp lines.&rdquo; <br /> &mdash;A. R. Ammons',
+        '&ldquo;Hey sky, take off your hat, I&rsquo;m on my way!&rdquo; <br /> &mdash;Valentina Tereshkova'
     ]
 
     let augustArray = [
