a6cf4126835ae1658441ffff6c3b44eb0c1641e1
serenascalzi <serenascalzi@alumni.iu.edu>
2021-09-05 15:06:48 -0700

    add september 5 quotes

--- js/funnyQuotes.js
+++ js/funnyQuotes.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
         '&ldquo;There is still no cure for the common birthday.&rdquo; <br /> &mdash;John Glenn',
         '&ldquo;I installed a skylight in my apartment&hellip; the people who live above me are furious!&rdquo; <br /> &mdash;Steven Wright',
         '&ldquo;I am fond of pigs. Dogs look up to us. Cats look down on us. Pigs treat us as equals.&rdquo; <br /> &mdash;Winston Churchill',
-        '&ldquo;If voting changed anything, they&rsquo;d make it illegal.&rdquo; <br /> &mdash;Emma Goldman'
+        '&ldquo;If voting changed anything, they&rsquo;d make it illegal.&rdquo; <br /> &mdash;Emma Goldman',
+        '&ldquo;If you cannot get rid of the family skeleton, you may as well make it dance.&rdquo; <br /> &mdash;George Bernard Shaw'
     ]
 
     let octoberArray = [
