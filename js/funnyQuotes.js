0896e23d21bf2e44cfbfe2d4cfd0619d4aced4de
serenascalzi <serenascalzi@alumni.iu.edu>
2021-09-16 13:09:08 -0700

    add september 15 quotes

--- js/funnyQuotes.js
+++ js/funnyQuotes.js
@@ -16,7 +16,8 @@ $(document).ready(function() {
         '&ldquo;Television is a medium because anything well done is rare.&rdquo; <br /> &mdash;Fred Allen',
         '&ldquo;I like long walks, especially when they are taken by people who annoy me.&rdquo; <br /> &mdash;Noel Coward',
         '&ldquo;Never trust a husband too far, nor a bachelor too near.&rdquo; <br /> &mdash;Helen Rowland',
-        '&ldquo;Never lend your car to anyone to whom you have given birth.&rdquo; <br /> &mdash;Erma Bombeck'
+        '&ldquo;Never lend your car to anyone to whom you have given birth.&rdquo; <br /> &mdash;Erma Bombeck',
+        '&ldquo;The politicians were talking themselves red, white and blue in the face.&rdquo; <br /> &mdash;Clare Boothe Luce'
     ]
 
     let octoberArray = [
