36362c85eddacdfa09d00d6dbe99c50ae536cc03
serenascalzi <serenascalzi@alumni.iu.edu>
2021-10-10 22:50:26 -0400

    add october 10 quotes

--- js/funnyQuotes.js
+++ js/funnyQuotes.js
@@ -10,7 +10,8 @@ $(document).ready(function() {
         '&ldquo;Nobody ever forgets where he buried the hatchet.&rdquo; <br /> &mdash;Kin Hubbard',
         '&ldquo;When a man tells you that he got rich through hard work, ask him: &lsquo;Whose?&rsquo;&rdquo; <br /> &mdash;Don Marquis',
         '&ldquo;All the world&rsquo;s a stage and most of us are desperately unrehearsed.&rdquo; <br /> &mdash;Sean O&rsquo;Casey',
-        '&ldquo;Christopher Columbus, as everyone knows, is honored by posterity because he was the last to discover America.&rdquo; <br /> &mdash;James Joyce'
+        '&ldquo;Christopher Columbus, as everyone knows, is honored by posterity because he was the last to discover America.&rdquo; <br /> &mdash;James Joyce',
+        '&ldquo;I used to sell furniture for a living. The trouble was, it was my own.&rdquo; <br /> &mdash;Les Dawson'
     ]
 
     let novemberArray = [
