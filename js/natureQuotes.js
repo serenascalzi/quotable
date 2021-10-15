f52230ac8a91997e3dc581adc1dda397175254a1
serenascalzi <serenascalzi@alumni.iu.edu>
2021-10-15 19:35:20 -0400

    add october 15 quotes

--- js/natureQuotes.js
+++ js/natureQuotes.js
@@ -15,7 +15,8 @@ $(document).ready(function() {
         '&ldquo;Human life is as evanescent as the morning dew or a flash of lightning.&rdquo; <br /> &mdash;Samuel Butler',
         '&ldquo;We forget that the water cycle and the life cycle are one.&rdquo; <br /> &mdash;Jacques Yves Cousteau',
         '&ldquo;I&rsquo;m very gregarious, but I love being in the hills on my own.&rdquo; <br /> &mdash;Norman MacCaig',
-        '&ldquo;Man is a creative retrospection of nature upon itself.&rdquo; <br /> &mdash;Karl Wilhelm Friedrich Schlegel'
+        '&ldquo;Man is a creative retrospection of nature upon itself.&rdquo; <br /> &mdash;Karl Wilhelm Friedrich Schlegel',
+        '&ldquo;And when I breathed, my breath was lightning.&rdquo; <br /> &mdash;Black Elk'
     ]
 
     let novemberArray = [
