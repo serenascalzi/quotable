21d1034fc9ce3360b25c27e2f065f4664540a5a2
serenascalzi <serenascalzi@alumni.iu.edu>
2021-10-27 13:13:37 -0400

    add october 27 quotes

--- js/funnyQuotes.js
+++ js/funnyQuotes.js
@@ -27,7 +27,8 @@ $(document).ready(function() {
         '&ldquo;Most of what we call management consists of making it difficult for people to get their work done.&rdquo; <br /> &mdash;Peter Drucker',
         '&ldquo;All I ask is the chance to prove that money can&rsquo;t make me happy.&rdquo; <br /> &mdash;Spike Milligan',
         '&ldquo;Bachelors have consciences, married men have wives.&rdquo; <br /> &mdash;Samuel Johnson',
-        '&ldquo;An Englishman teaching an American about food is like the blind leading the one-eyed.&rdquo; <br /> &mdash;A. J. Liebling'
+        '&ldquo;An Englishman teaching an American about food is like the blind leading the one-eyed.&rdquo; <br /> &mdash;A. J. Liebling',
+        '&ldquo;I have an unfortunate personality.&rdquo; <br /> &mdash;Orson Welles'
     ]
 
     let novemberArray = [
