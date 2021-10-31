aac378951c8dcd32c4fc3cb4ab1b54ea40169cbf
serenascalzi <serenascalzi@alumni.iu.edu>
2021-10-31 15:21:19 -0400

    add october 29 quotes

--- js/natureQuotes.js
+++ js/natureQuotes.js
@@ -29,7 +29,8 @@ $(document).ready(function() {
         '&ldquo;The sea has neither meaning nor pity.&rdquo; <br /> &mdash;Anton Chekhov',
         '&ldquo;Wilderness is not a luxury but a necessity of the human spirit.&rdquo; <br /> &mdash;Edward Abbey',
         '&ldquo;You forget that the fruits belong to all and that the land belongs to no one.&rdquo; <br /> &mdash;Jean-Jacques Rousseau',
-        '&ldquo;Without the oceans there would be no life on Earth.&rdquo; <br /> &mdash;Peter Benchley'
+        '&ldquo;Without the oceans there would be no life on Earth.&rdquo; <br /> &mdash;Peter Benchley',
+        '&ldquo;But more wonderful than the lore of old men and the lore of books is the secret lore of ocean.&rdquo; <br /> &mdash;H. P. Lovecraft'
     ]
 
     let novemberArray = [
