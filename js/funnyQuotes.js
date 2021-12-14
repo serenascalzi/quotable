6e5e2ea811403d8391a5c7841f866035e4ab1c0e
serenascalzi <serenascalzi@alumni.iu.edu>
2021-12-14 11:14:52 -0800

    add december 14 quotes

--- js/funnyQuotes.js
+++ js/funnyQuotes.js
@@ -14,7 +14,8 @@ $(document).ready(function() {
         '&ldquo;You wouldn&rsquo;t have won if we&rsquo;d beaten you.&rdquo; <br /> &mdash;Yogi Berra',
         '&ldquo;The best measure of a man&rsquo;s honesty isn&rsquo;t his income tax return. It&rsquo;s the zero adjust on his bathroom scale.&rdquo; <br /> &mdash;Arthur C. Clarke',
         '&ldquo;First you take a drink, then the drink takes a drink, then the drink takes you.&rdquo; <br /> &mdash;F. Scott Fitzgerald',
-        '&ldquo;Always do sober what you said you&rsquo;d do drunk. That will teach you to keep your mouth shut.&rdquo; <br /> &mdash;Ernest Hemingway'
+        '&ldquo;Always do sober what you said you&rsquo;d do drunk. That will teach you to keep your mouth shut.&rdquo; <br /> &mdash;Ernest Hemingway',
+        '&ldquo;I see nothing in space as promising as the view from a Ferris wheel.&rdquo; <br /> &mdash;E. B. White'
     ]
 
     let januaryArray = [
