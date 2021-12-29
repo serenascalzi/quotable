b1deec69f3cc88f7e369261666e2bdfb100c13eb
serenascalzi <serenascalzi@alumni.iu.edu>
2021-12-28 22:43:09 -0800

    add december 28 quotes

--- js/funnyQuotes.js
+++ js/funnyQuotes.js
@@ -28,7 +28,8 @@ $(document).ready(function() {
         '&ldquo;Santa Claus has the right idea &ndash; visit people only once a year.&rdquo; <br /> &mdash;Victor Borge',
         '&ldquo;What I am looking for is a blessing not in disguise.&rdquo; <br /> &mdash;Jerome K. Jerome',
         '&ldquo;Please to put a nickel, please to put a dime. How petitions trickle in at Christmas time!&rdquo; <br /> &mdash;Phyllis McGinley',
-        '&ldquo;I come from a family where gravy is considered a beverage.&rdquo; <br /> &mdash;Erma Bombeck'
+        '&ldquo;I come from a family where gravy is considered a beverage.&rdquo; <br /> &mdash;Erma Bombeck',
+        '&ldquo;Nothing says holidays, like a cheese log.&rdquo; <br /> &mdash;Ellen DeGeneres'
     ]
 
     let januaryArray = [
